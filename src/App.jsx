d714259969f2619987a4357156c08d4676ce457e
Puri <purificacion.bastida@gmail.com>
2024-02-24 17:06:28 +0100

    Modificar class app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 
 function App() {  
-  return <h1 className="text-3l font-bold underline bg-red-200">Hello world!</h1>
+  return (
+    <h1 className="text-3l font-bold underline bg-red-200">
+      Hello world!
+      <div className="bg-blue-200 flex justify-between">        
+        <div className="bg-red-100 w-fit">Soy el hijo</div>
+        <div className="bg-red-100 w-fit">Soy el hijo</div>
+        <div className="bg-red-100 w-fit">Soy el hijo</div>     
+      </div>
+  </h1>
+  )
 }
 
 export default App
