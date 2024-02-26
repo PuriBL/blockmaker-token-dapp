b78613f81b42e2d49dbd8c665cd2f47b9276a1cb
Puri <purificacion.bastida@gmail.com>
2024-02-26 23:59:26 +0100

    Añadir componente Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,7 @@
 export default function Footer(){
-    return <footer>Esto es el Footer</footer>
+    return (
+    <footer className="border-t py-4 px-3 flex justify-center bg-white shadow-x5">
+        <p>Derechos de autor &copy; {new Date().getFullYear()} Blockmaker Academy</p>
+        </footer>
+    )
 }
