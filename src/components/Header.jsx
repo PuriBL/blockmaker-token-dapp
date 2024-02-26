f07560cea7ce086c14d2a0666e693a45fed8ab8c
Puri <purificacion.bastida@gmail.com>
2024-02-26 23:59:00 +0100

    Añadir componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 export default function Header() {
     return (
-        <header className="bg-red-200 py-2 px-3">
-            {/*logo para versión mobile aclaración*/}
-            <img src="/logo-blockmaker grande.png" alt="blockmaker-desktop-logo" />  
-            <img src="/Blockmaker logo.png" alt="blockmaker-mobile-logo" />
-            {/*logo para versión desktop aclaración*/}           
+        <header className="border-b shadow py-2 px-3 flex justify-between items-center">
+            {/*logo para versión desktop aclaración*/}
+            <img src="/logo-blockmaker grande.png" alt="blockmaker-desktop-logo"
+            className="hidden sm:block" width={300} />  
+            <img src="/Blockmaker logo.png" alt="blockmaker-mobile-logo" className="sm:hidden" width={47}/>
+            {/*logo para versión mobile aclaración*/}    
+            <button className="bg-gray-100 rounded-xl px-2 text-sm h-fit py-2">Connect Wallet</button>       
         </header>
     )
 }
