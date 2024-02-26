0a90f78be39be57f7b9facad537222ba02f0a1d3
Puri <purificacion.bastida@gmail.com>
2024-02-26 22:27:09 +0100

    Añadir logos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,11 @@
-export default function Header(){
-    return <header className="bg-red-200 py-2"> Esto es el Header</header>
+export default function Header() {
+    return (
+        <header className="bg-red-200 py-2 px-3">
+            {/*logo para versión mobile aclaración*/}
+            <img src="/logo-blockmaker grande.png" alt="blockmaker-desktop-logo" />  
+            <img src="/Blockmaker logo.png" alt="blockmaker-mobile-logo" />
+            {/*logo para versión desktop aclaración*/}           
+        </header>
+    )
 }
 
