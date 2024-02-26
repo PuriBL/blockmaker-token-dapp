0626f1652d70274e1050785365d328737df2295e
Puri <purificacion.bastida@gmail.com>
2024-02-27 00:20:56 +0100

    Actualizar componente app con Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import {AppLayout} from './components/ui/layouts'
+import {Home} from './pages'
 
 function App(){
   return (
    <AppLayout>
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <Home />
    </AppLayout> 
   )
 }
