720ae8252c765b5faec376271892977236e7e950
Puri <purificacion.bastida@gmail.com>
2024-03-02 07:35:18 +0100

    Actualizar componente Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,7 @@
-import {useAccount } from 'wagmi'
+import { useAccount } from 'wagmi'
+import { Owner } from '../components'
+import { LoadingSpinner, Button, ErrorInfo, TextInput, Title } from '../components/ui'
+import { ConnectKitButton } from 'connectkit'
 
 export default function Home(){
     const {address, isConnecting, isDisconnected } = useAccount()
@@ -6,7 +9,20 @@ export default function Home(){
     if (isConnecting) return <div>Connecting ... </div>
     if (isDisconnected) return <div>Disconnected</div>
 
-    return <div>Connected Wallet: {address}</div>         
-    
+    return (
+        <div className= 'flex flex-col gap-6'>      
+            <div>Connected Wallet: {address}</div>      
+            <LoadingSpinner className="h-12 w-12" />
+            <div>
+                <Button>Comprar Tokens</Button>
+            </div>  
+            <ErrorInfo message="Error: Internal Server Error"/>  
+            <div>
+                <TextInput />   
+            </div> 
+            <Title>Este es un titulo de muestra</Title>     
+            <Owner />   
+        </div>        
+    )    
 }
 
