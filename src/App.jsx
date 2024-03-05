05e66fc3f842524b89180b9e0e2e0a22bba9c9bc
Puri <purificacion.bastida@gmail.com>
2024-03-05 02:04:39 +0100

    Actualizar App

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import {ConnectKitProvider} from 'connectkit'
-import {WagmiConfig} from 'wagmi'
-import {AppLayout} from './components/ui/layouts'
-import {config} from './config/wagmi'
-import {Home} from './pages'
+import { ConnectKitProvider } from 'connectkit'
+import { WagmiConfig } from 'wagmi'
+import { AppLayout } from './components/ui/layouts'
+import { config } from './config/wagmi'
+import { Home } from './pages'
+import { Toaster } from 'react-host-toast'
 
-
-
-function App(){  
+function App() {
   return (
-   <WagmiConfig config={config}>
-    <ConnectKitProvider mode="light">
-      <AppLayout>
-        <Home/>              
-      </AppLayout> 
-    </ConnectKitProvider>
-   </WagmiConfig>
+    <WagmiConfig config={config}> 
+      <Toaster position="botton-right"/>   
+      <ConnectKitProvider mode="light">
+        <AppLayout>
+          <Home />
+        </AppLayout>
+      </ConnectKitProvider>
+    </WagmiConfig>
   )
 }
+
 export default App
+
+
 
 
 
